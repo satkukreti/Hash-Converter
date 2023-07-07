6f237a53f0afde8d32d53de6ddf35504ad30d5d6
Satvik Kukreti <65563494+satkukreti@users.noreply.github.com>
2023-07-07 16:07:35 -0400

    Allowed it to work for mulitple files without rerunning

--- convertHashes.py
+++ convertHashes.py
@@ -1,3 +1,5 @@
+#Author: Satvik Kukreti
+
 import requests
 import tkinter as tk
 from tkinter import ttk
@@ -8,23 +10,23 @@ headers = {
     "x-apikey": "" #Enter your API key from your VirusTotal Account
 }
 
-'''Gets the file path: uses a graphical interface'''
-def open_text_file():
+
+'''Gets the file path using a graphical interface'''
+def open_text_file(progress):
+    t.delete("1.0", "end")
+    progress = "Getting file: "
+    t.insert(tk.END, progress);
     filetypes = (('text files', '*.txt'), ('All files', '*.*'))
     try:
         f = fd.askopenfilename(filetypes=filetypes, initialdir="D:/Downloads")
         convertHash(f)
     except FileNotFoundError as e:
         progress = "File not found or selected"
-        t.delete("1.0", "end")
         t.insert(tk.END, progress)
 
 '''Converts the hashes'''
 def convertHash(filepath):
-    nfile = open("sha1conversion.txt", "w")
-    progress = "Getting file " + filepath
-    t.delete("1.0", "end")
-    t.insert(tk.END, progress)
+    nfile = open("hashconverstion.txt", "w")
     with open(filepath, 'r') as f:
         for line in f:
             h = line.strip()
@@ -39,8 +41,7 @@ def convertHash(filepath):
 
             except Exception as e:
                 nfile.write("Not Found\n")
-    progress = progress + " ...DONE"
-    t.delete("1.0", "end")
+    progress = filepath + " - " + clicked.get()
     t.insert(tk.END, progress)
 
 '''Creating the GUI'''
@@ -51,10 +52,9 @@ app.geometry("300x150")
 l = tk.Label(app, font=("Calibri", 12), text="Select a file and hash type", height=10)
 l.place(relx=0.50, rely=0.15, anchor="center")
 
-progress = "Please select a text file"
-
 t = tk.Text(app, height=2, width=35, wrap="char")
 t.place(relx=0.50, rely=0.40, anchor="center")
+progress = "Please select a text file"
 t.insert(tk.END, progress)
 
 clicked = tk.StringVar(app)
@@ -62,6 +62,6 @@ clicked.set("SHA256")
 drop = tk.OptionMenu(app, clicked, "SHA256", "MD5")
 drop.place(relx=0.70, rely=0.75, anchor="center")
 
-open_button = ttk.Button(app, text="Choose File", command=open_text_file)
+open_button = ttk.Button(app, text="Choose File", command=lambda: open_text_file(progress))
 open_button.place(relx=0.30, rely=0.75, anchor="center")
 app.mainloop()
